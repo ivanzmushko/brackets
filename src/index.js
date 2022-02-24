c3338141176cb75890163864a4494686cca74132
Ivan <80452629+ivanzmushko@users.noreply.github.com>
2022-02-24 21:34:57 +0300

    fiat: add script.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let Stack = [];
+
+  for (let bracket of str) 
+    {
+      let bracketsIndex = brackets.indexOf(bracket);
+      if (bracketsIndex % 2 === 0) 
+      {
+          if (bracket === brackets[bracketsIndex + 1] && Stack[Stack.length - 1] === bracketsIndex) {
+              Stack.pop();
+          }
+          else if (bracket === brackets[bracketsIndex + 1] && Stack[Stack.length - 1] !== bracketsIndex) 
+          {
+              Stack.push(bracketsIndex);
+          }
+          else 
+          {
+              Stack.push(bracketsIndex);
+          }
+      }  
+      else 
+      {
+          if (Stack.pop() !== bracketsIndex-1) 
+          {
+              return false;
+          }
+      }
+      
+  }
+  return Stack.length === 0;
 }
